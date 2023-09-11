787884e97e35d410c00425e7ecdb62a5cb25f259
shoaib-fateh <shoaibfateh21@yahoo.com>
2023-09-11 16:44:29 +0430

    add new styles for buttons and update app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,40 @@
 
-function Square({ value }) {
-	return <button className="square">{ value }</button>
+import { useState } from "react"
+
+function Square({value, onSquareClick }) {
+	return (
+		<button
+			className="square" 
+			onClick={onSquareClick }>
+				{ value }
+		</button>
+	)
 }
 
 export default function Board(){
+	const [squares, setSquares] = useState(Array(9).fill(null))
+
+	function handleClick(i) {
+		const nextSquares = squares.slice();
+		nextSquares[i]="X";
+		setSquares(nextSquares)
+	}
+
 	return <>
 		<div className="board-row">
-			<Square value="1" />
-			<Square value="2" />
-			<Square value="3" />
+			<Square value={squares[1]} onSquareClick={()=> handleClick(1)} />
+			<Square value={squares[2]} onSquareClick={()=> handleClick(2)} />
+			<Square value={squares[3]} onSquareClick={()=> handleClick(3)} />
 		</div>
 		<div className="board-row">
-			<Square value="4" />
-			<Square value="5" />
-			<Square value="6" />
+			<Square value={squares[4]} onSquareClick={()=> handleClick(4)} />
+			<Square value={squares[5]} onSquareClick={()=> handleClick(5)} />
+			<Square value={squares[6]} onSquareClick={()=> handleClick(6)} />
 		</div>
 		<div className="board-row">
-			<Square value="7" />
-			<Square value="8" />
-			<Square value="9" />
+			<Square value={squares[7]} onSquareClick={()=> handleClick(7)} />
+			<Square value={squares[8]} onSquareClick={()=> handleClick(8)} />
+			<Square value={squares[9]} onSquareClick={()=> handleClick(9)} />
 		</div>
 	</>
 }
