b152ae2f1d9116d0085683b4d8c1825bbd97c812
shoaib-fateh <shoaibfateh21@yahoo.com>
2023-09-11 19:14:34 +0430

    Adding time travel

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
-import { createRoot } from "react-dom/client"
+import { createRoot } from "react-dom/client";
 
 import "./styles.css";
 
-import App from "./App"
+import App from "./App";
 
-const rootElement = document.getElementById("root")
-createRoot(rootElement).render(<App />)
+const rootElement = document.getElementById("root");
+createRoot(rootElement).render(<App />);
