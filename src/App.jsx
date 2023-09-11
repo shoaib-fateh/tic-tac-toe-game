3e3b5365c23b34bbddd72a17728796223936639f
shoaib-fateh <shoaibfateh21@yahoo.com>
2023-09-11 16:00:20 +0430

    workspace - ready.

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+/** @format */
+
+import React, { Component } from 'react';
+
+class App extends Component {
+	render() {
+		return (
+			<>
+			<h1>Tic-Tac-Toe Project!</h1>
+			</>
+		);
+	}
+}
+
+export default App;
