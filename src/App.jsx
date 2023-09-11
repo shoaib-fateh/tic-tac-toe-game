379b84ce21740fdb9d453ec4e4a6b6c6ff3145de
shoaib-fateh <shoaibfateh21@yahoo.com>
2023-09-11 19:15:03 +0430

    Final cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -84,21 +84,19 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-	const [xIsNext, setXIsNext] = useState(true);
 	const [history, setHistory] = useState([Array(9).fill(null)]);
 	const [currentMove, setCurrentMove] = useState(0);
+	const xIsNext = currentMove % 2 === 0;
 	const currentSquares = history[currentMove];
 
 	function handlePlay(nextSquares) {
 		const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
 		setHistory(nextHistory);
 		setCurrentMove(nextHistory.length - 1);
-		setXIsNext(!xIsNext);
 	}
 
 	function jumpTo(nextMove) {
 		setCurrentMove(nextMove);
-		setXIsNext(nextMove % 2 === 0);
 	}
 
 	const moves = history.map((squares, move) => {
