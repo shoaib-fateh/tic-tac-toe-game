3ce93f0d1e9eb12b95dd06f2730194fabf6fb691
shoaib-fateh <shoaibfateh21@yahoo.com>
2023-09-11 16:56:26 +0430

    Taking turns

--- src/App.jsx
+++ src/App.jsx
@@ -13,11 +13,18 @@ function Square({value, onSquareClick }) {
 
 export default function Board(){
 	const [squares, setSquares] = useState(Array(9).fill(null))
+	const [xIsNext, setXIsNext] = useState(true)
 
 	function handleClick(i) {
+		if(squares[i]) return
 		const nextSquares = squares.slice();
-		nextSquares[i]="X";
+		if (xIsNext) {
+			nextSquares[i]="X";
+		} else {
+			nextSquares[i] = "O"
+		}
 		setSquares(nextSquares)
+		setXIsNext(!xIsNext)
 	}
 
 	return <>
